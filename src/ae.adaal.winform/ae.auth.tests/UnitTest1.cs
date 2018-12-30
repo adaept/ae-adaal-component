711a97da5ebb4a110757964ba76b048c398ac258
peterennis <peterennis@yahoo.com>
2018-12-29 21:25:55 -0800

    fix () remove old and use only adaal

---
+++ src/ae.adaal.winform/ae.auth.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ae.auth.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/ae.adaal.winform/ae.auth/Helpers/NativeMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ae.auth.Helpers
+{
+    #region NativeMethods
+    public static class NativeMethods
+    {
+        [DllImport("NetApi32.dll", CharSet = CharSet.Unicode)]
+        public static extern int DsGetDcNameW(
+            IntPtr ComputerName,
+            string DomainName,
+            IntPtr DomainGuid,
+            IntPtr SiteName,
+            DsFlag Flags,
+            out IntPtr DomainControllerInfo);
+
+        [DllImport("NetApi32.dll", CharSet = CharSet.Unicode)]
+        public static extern int DsGetDcNameW(
+            string ComputerName,
+            string DomainName,
+            IntPtr DomainGuid,
+            IntPtr SiteName,
+            DsFlag Flags,
+            out IntPtr DomainControllerInfo);
+
+        [DllImport("NetApi32.dll", CharSet = CharSet.Unicode)]
+        public static extern int DsGetDcOpenW(
+            string DnsName,
+            DsEnumerateOptions options,
+            IntPtr SiteName,
+            IntPtr DomainGuid,
+            IntPtr DnsForestName,
+            DsFlag Flags,
+            out IntPtr GetDcContext);
+
+        [DllImport("NetApi32.dll", CharSet = CharSet.Unicode)]
+        public static extern int DsGetDcNextW(
+            IntPtr GetDcContext,
+            IntPtr SockAddressCount,
+            IntPtr SockAddresses,
+            out IntPtr DnsHostName);
+
+        [DllImport("NetApi32.dll", CharSet = CharSet.Unicode)]
+        public static extern int NetApiBufferFree(IntPtr bufptr);
+
+        [DllImport("NetApi32.dll", CharSet = CharSet.Unicode)]
+        public static extern void DsGetDcCloseW(IntPtr GetDcContext);
+    }
+    #endregion
+}
